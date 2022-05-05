0465fa662a257ec1b9348b305d38febd36c37fc6
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-05-05 16:51:59 +0530

    Update depend.sh

--- depend.sh
+++ depend.sh
@@ -3,6 +3,6 @@
 cd /home/ubuntu
 sudo apt update
 sudo apt install python3-pip python3-dev nginx -y
-pip install -r requirements.txt
+pip install django gunicorn
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
