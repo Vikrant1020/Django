93df32861d642905404f110305100fed5c0599d3
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-05-09 20:29:19 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -37,7 +37,7 @@ sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 54.95.234.19;
+    server_name 54.199.155.202;
 
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
