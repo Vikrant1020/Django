f601f93095fb8c87395fa357c039ecb9f42853d2
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-05-11 01:05:22 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,52 +3,19 @@
 #pip install -r requirements.txt
 deactivate
 
-echo "[Unit]
-Description=gunicorn socket
-
-[Socket]
-ListenStream=/run/gunicorn.sock
-
-[Install]
-WantedBy=sockets.target
-" > /etc/systemd/system/gunicorn.socket
-
-echo " [Unit]
-Description=gunicorn daemon
-Requires=gunicorn.socket
-After=network.target
-
-[Service]
-User=ubuntu
-Group=www-data
-WorkingDirectory=/home/ubuntu/
-ExecStart=/home/ubuntu/env/bin/gunicorn \
-          --access-logfile - \
-          --workers 3 \
-          --bind unix:/run/gunicorn.sock \
-          first.wsgi:application
-
-[Install]
-WantedBy=multi-user.target"  >  /etc/systemd/system/gunicorn.service
-
-sudo systemctl start gunicorn.socket
-
-sudo systemctl enable gunicorn.socket
-
+gunicorn --bind 0.0.0.0:8000 first.wsgi &
 
 echo "server {
-    listen 80;
-    server_name 35.78.64.166;
+          listen 80;
+          server_name 18.183.2.22;
 
-    location = /favicon.ico { access_log off; log_not_found off; }
-    location /static/ {
-        root /home/ubuntu/;
-    }
+          location /static/ {
+                    root /home/ubuntu/static/;
+          }        
 
-    location / {
-        include proxy_params;
-        proxy_pass http://unix:/run/gunicorn.sock;
-    }
+          location / {
+          proxy_pass http://18.183.2.22:8000;
+          }
 }" > /etc/nginx/sites-available/first
 
 
