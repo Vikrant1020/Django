c1e0f138610c1603893cb7a0a8e7669655c99144
Vikrant1020 <98014253+Vikrant1020@users.noreply.github.com>
2022-05-11 16:14:38 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,23 +7,8 @@ sudo fuser -k 8000/tcp
 
 gunicorn --bind 0.0.0.0:8000 first.wsgi &>/dev/null & 
 
-echo "server {
-listen 80;
-server_name 18.183.2.22;
 
-location /static/ {
-root /home/ubuntu/static/;
- }
-
-location/{
-
-proxy_pass http://18.183.2.22:8000;
- }
-
-}" > /etc/nginx/sites-available/Nginx
-
-
-#mv /home/ubuntu/NGINX /etc/nginx/sites-available 
+mv /home/ubuntu/NGINX /etc/nginx/sites-available 
 
 sudo ln -s /etc/nginx/sites-available/NGINX /etc/nginx/sites-enabled/
 
